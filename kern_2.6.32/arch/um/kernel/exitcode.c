4a459e7329a7b717ae924ccea5fc1c0ab5154854
argentinos <argentinos@users.noreply.github.com>
2014-06-02 23:41:03 +0300

    Update exitcode.c
    update from www.kernel.org

--- kern_2.6.32/arch/um/kernel/exitcode.c
+++ kern_2.6.32/arch/um/kernel/exitcode.c
@@ -44,7 +44,8 @@ static int write_proc_exitcode(struct file *file, const char __user *buffer,
 	char *end, buf[sizeof("nnnnn\0")];
 	int tmp;
 
-	if (copy_from_user(buf, buffer, count))
+	size = min(count, sizeof(buf));
+	if (copy_from_user(buf, buffer, size)) 
 		return -EFAULT;
 
 	tmp = simple_strtol(buf, &end, 0);
